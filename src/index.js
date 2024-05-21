7b1f2a4cc05b43dc3a666cdae820c638e8bd1500
Felipe Miranda de Lima <fmlima4@outlook.com>
2024-05-21 17:46:11 -0300

    pwa talvez

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -12,3 +24,4 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
